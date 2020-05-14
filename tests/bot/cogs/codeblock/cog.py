92e3b44c8709d95f83559d77c3a10f315371ba2c
MarkKoz <KozlovMark@gmail.com>
2020-05-13 18:45:34 -0700

    Test CodeBlockCog.__init__

--- tests/bot/cogs/codeblock/cog.py
+++ tests/bot/cogs/codeblock/cog.py
@@ -18,3 +18,17 @@ class CodeBlockExtensionTests(unittest.TestCase):
         cog.assert_called_once_with(bot)
         bot.add_cog.assert_called_once()
         self.assertTrue(isinstance(bot.add_cog.call_args.args[0], CodeBlockCog))
+
+
+class CodeBlockCogTests(unittest.IsolatedAsyncioTestCase):
+    """Tests for the CodeBlock cog."""
+
+    def setUp(self):
+        self.bot = helpers.MockBot()
+
+    @mock.patch("bot.constants.CodeBlock.cooldown_channels", new=[1, 2])
+    def test_init(self):
+        """Bot should be saved as an attribute and channel cooldowns should be set to 0."""
+        cog = CodeBlockCog(self.bot)
+        self.assertEqual(cog.bot, self.bot)
+        self.assertDictEqual(cog.channel_cooldowns, {1: 0.0, 2: 0.0})
